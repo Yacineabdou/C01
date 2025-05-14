103d6164c02d101babb6c8e8fbf650db59555fe0
yacine <issay584@email.com>
2025-05-14 05:13:31 +0100

    git commit

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -5,6 +5,6 @@ compteur=1
 
 # Parcourir tous les arguments avec $@
 for arg in "$@"; do
-    echo "Argument $compteur: $arg"
+    echo "Argument $compteur : $arg"
     ((compteur++))
 done
--- find_string.sh
+++ find_string.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # Nom du fichier dans lequel effectuer la recherche
-FICHIER="ficher.txt"
+read f
+
+# Chaîne de caractères à rechercher
+read c
+
+# Vérifier si la chaîne a été trouvée
+if  grep -q "$c" "$f"; then
+  echo "La chaîne '$c' a été trouvée dans $f."
+else
+  echo "La chaîne '$c' n'a pas été trouvée dans $f."
+fi
